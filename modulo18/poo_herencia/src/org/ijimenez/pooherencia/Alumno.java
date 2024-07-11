759c961e5439e7f80765cf0dac3131b891c73475
Isael Jimenez <isaeljimenez94@gmail.com>
2024-07-10 21:01:29 -0600

    feat: Herencia
    Este es un ejemplo con herencia

--- modulo18/poo_herencia/src/org/ijimenez/pooherencia/Alumno.java
+++ modulo18/poo_herencia/src/org/ijimenez/pooherencia/Alumno.java
@@ -1,4 +1,40 @@
 package org.ijimenez.pooherencia;
 
-public class Alumno {
+public class Alumno extends Persona{
+    private String institucion;
+    private double notaMatematica;
+    private double notaLengua;
+    private double notaHistoria;
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+
+    public double getNotaMatematica() {
+        return notaMatematica;
+    }
+
+    public void setNotaMatematica(double notaMatematica) {
+        this.notaMatematica = notaMatematica;
+    }
+
+    public double getNotaLengua() {
+        return notaLengua;
+    }
+
+    public void setNotaLengua(double notaLengua) {
+        this.notaLengua = notaLengua;
+    }
+
+    public double getNotaHistoria() {
+        return notaHistoria;
+    }
+
+    public void setNotaHistoria(double notaHistoria) {
+        this.notaHistoria = notaHistoria;
+    }
 }
